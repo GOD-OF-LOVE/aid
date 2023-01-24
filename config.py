ee4436bda6dde7f2572f547fa5fb8a243557d845
MUKESH BABU S <108213299+GOD-OF-LOVE@users.noreply.github.com>
2023-01-24 10:05:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ OWNER_ID = int(getenv("OWNER_ID", "5354500761"))
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://karthika:karthika@cluster0.6jms6m3.mongodb.net/?retryWrites=true&w=majority")
 BOT_TOKEN = getenv("BOT_TOKEN", "5941662739:AAEQ56IVr9aOCrApC1VwwqPGfkzTqX5a1-k")
 ALIVE_PIC = getenv("ALIVE_PIC", "https://telegra.ph/file/3870413e25d66bbfacf43.jpg")
-ALIVE_TEXT = getenv("ALIVE_TEXT", "Im Alone x UserBot"
+ALIVE_TEXT = getenv("ALIVE_TEXT", "Im Alone x UserBot")
 PM_LOGGER = getenv("PM_LOGGER", "-1001522073183")
 LOG_GROUP = getenv("LOG_GROUP", "-1001888105987")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
